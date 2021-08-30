9a91dc79e84b0346d1741a209d1c8e5f43422e73
rickychow2006 <rickychow0716@gmail.com>
2021-08-30 13:32:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let Main = document.querySelector("#main").remove();
+Main;
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);
